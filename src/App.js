5595860342747d2ac7c84be37fac9c3477258d53
kimseungmok <kimseungmok@naver.com>
2021-08-06 07:49:48 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers = [
+  {
+  'id' :1,
+  'image':'https://placeimg.com/64/64/1',
+  'name' : 'kimseungmok',
+  'birthday' : '19950823',
+  'gender' : 'man',
+  'job' : 'programmer'
+},
+{
+  'id' :2,
+  'image':'https://placeimg.com/64/64/2',
+  'name' : 'yuna',
+  'birthday' : '19970815',
+  'gender' : 'woman',
+  'job' : 'services'
+},
+{
+  'id' :3,
+  'image':'https://placeimg.com/64/64/3',
+  'name' : 'mi-tan',
+  'birthday' : '19970000',
+  'gender' : 'woman',
+  'job' : 'services'
+},
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+        {
+          customers.map(customerInfo => {
+            return(
+            <Customer
+              key={customerInfo.id}
+
+              id={customerInfo.id}
+              image={customerInfo.image}
+              name ={customerInfo.name}
+              birthday ={customerInfo.birthday}
+              gender ={customerInfo.gender}
+              job ={customerInfo.job}
+            />
+            );
+          })
+        }
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
