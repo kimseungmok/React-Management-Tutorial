f8fe030aed7c36f244f6b087724e8fc2e9c07a45
kimseungmok <kimseungmok@naver.com>
2021-08-06 16:26:33 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import logo from './logo.svg';
+import React,{ Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import { withStyles } from '@material-ui/core/styles';
+import { classes } from 'istanbul-lib-coverage';
+
+const styles = theme => ({
+  root: {
+    width : '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflow: "auto"
+  },
+  table: {
+    minWidth : 1080
+  }
+})
 
 const customers = [
   {
@@ -29,31 +48,46 @@ const customers = [
 },
 ]
 
+class App extends Component {
+  render (){
 
-function App() {
-  return (
-    <div>
-        {
-          customers.map(customerInfo => {
-            return(
-            <Customer
-              key={customerInfo.id}
+    const { classes } = this.props;
 
-              id={customerInfo.id}
-              image={customerInfo.image}
-              name ={customerInfo.name}
-              birthday ={customerInfo.birthday}
-              gender ={customerInfo.gender}
-              job ={customerInfo.job}
-            />
-            );
-          })
-        }
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-    </div>
-  );
+    return(
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {
+            customers.map(customerInfo => {
+              return(
+              <Customer
+                key={customerInfo.id}
+
+                id={customerInfo.id}
+                image={customerInfo.image}
+                name ={customerInfo.name}
+                birthday ={customerInfo.birthday}
+                gender ={customerInfo.gender}
+                job ={customerInfo.job}
+              />
+              );
+            })
+          }
+          </TableBody>
+        </Table>
+    </Paper>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
